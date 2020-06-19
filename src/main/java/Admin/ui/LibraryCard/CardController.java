07d6601f202d59aa617e5262b0d6d71414dc8960
Andrea Mara <dalianamara@yahoo.com>
2020-06-19 14:35:33 +0300

    SDGB-49 Crearea unui fișier pentru persistarea credențialelor

---
+++ src/main/java/Admin/ui/LibraryCard/CardController.java
@@ -0,0 +1,64 @@
+package Admin.ui.LibraryCard;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+
+public class CardController implements Initializable {
+    @FXML
+    private JFXTextField username2;
+    @FXML
+    private JFXTextField ID;
+    @FXML
+    private JFXTextField username;
+
+
+    public void handleLoginButtonAction(ActionEvent actionEvent) throws InterruptedException {
+        //crearea fisierului pentru persistarea credențialelor
+        JSONObject obj = new JSONObject();
+        JSONArray jrr = new JSONArray();
+        JSONParser jp = new JSONParser();
+        try{
+            FileReader file = new FileReader("src/main/resources/Databases/Card.json");
+            jrr=(JSONArray)jp.parse(file);
+        }catch(Exception ex){
+
+        }
+
+
+        obj.put("Nume", username.getText());
+        obj.put("Prenume", username2.getText());
+        obj.put("ID", ID.getText());
+        jrr.add(obj);
+        try{
+            FileWriter file = new FileWriter("src/main/resources/Databases/Card.json");
+            file.write(jrr.toJSONString());
+            file.close();
+        }catch(Exception ex){
+
+        }
+
+
+    }
+
+    public void handleCancelButtonAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TO DO
+    }
+}
